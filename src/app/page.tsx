c3d3cbeeebe0ed6b2379ad2c1f714274b80a62a2
Rubab Bukhari <161203406+rubab1991@users.noreply.github.com>
2024-10-23 22:56:35 +0500

    second commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,4 @@
-"use client";
-import { useState, useEffect } from "react";
-import Image from "next/image";
-import Link from "next/link";
+
 import Hero from "./hero/page";
 
 export default function Home() {
